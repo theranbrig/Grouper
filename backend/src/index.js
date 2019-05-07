6efbe95ba2114ffae212246bbcdc5331c173ae33
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-05-08 01:05:20 +0900

    updated origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,7 +34,7 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin:[ process.env.FRONTEND_URL, 'http://localhost:19000/']
+      origin:[ process.env.FRONTEND_URL, 'http://localhost:19000/', '127.0.0.1:19000']
     },
   },
   deets => {
